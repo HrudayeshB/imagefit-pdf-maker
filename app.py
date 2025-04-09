ad5c0d86537a8c5eec0fcb99dd3a45ced2fb0a57
Hrudayesh Bhaskara <135438704+HrudayeshB@users.noreply.github.com>
2025-04-09 18:38:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(f"<script>{js_code}</script>", unsafe_allow_html=True)
 
 # Title
 st.markdown("<h1 style='text-align: center;'>ImageFit PDF Maker</h1>", unsafe_allow_html=True)
-st.markdown("<div style='text-align: center; font-size: 16px; opacity: 0.7;'></div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; font-size: 16px; opacity: 0.7;'>No more wasting paper - Fit your images smartly</div>", unsafe_allow_html=True)
 st.markdown("<hr style='border: 1px solid red;'>", unsafe_allow_html=True)
 
 uploaded_files = st.file_uploader(
